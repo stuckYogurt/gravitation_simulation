d3b0bc638e0294ced3d799c2fae3339473b199ae
 Владимир Малахов <vladimirmalakhov@MacBook-Air-Vladimir.local>
2024-12-16 15:37:34 +0300

    Fixed 2+ objects handling

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,10 @@ public:
         this->tail = 0;
     }
 
+    ~Tail() {
+        delete[] buffer;
+    }
+
 //    void push_back(int element)
 //    {
 //        buffer[head] = element;
@@ -102,10 +106,12 @@ unsigned short FPS = 500;
 unsigned short NUMBER_OF_OBJECTS = 2;
 unsigned short ORBIT_VISIBLE = 1000;
 double SCALE = 10000;
+double MOVE_IN_Y = 10; // in screen pixels
+double MOVE_IN_X = 10; // in screen pixels
 
 // physics constants
-//const double G_CONSTANT = 6.6743e-11;
-const double G_CONSTANT = 1;
+const double G_CONSTANT = 6.6743e-11;
+//const double G_CONSTANT = 1;
 
 
 
@@ -179,14 +185,25 @@ public:
         shape.setFillColor(color);
     }
 
+    ~Planet() {
+        delete orbit_tail;
+    }
+
 
     void move(sf::RenderWindow* window, double dx, double dy) {
         if (movable) {
             coordinate.x += dx;
             coordinate.y += dy;
-            shape.setPosition(coordinate.x, coordinate.y);
-            orbit_tail->push_front(&coordinate);
         }
+
+        coordinateStruct* screen_coord = new coordinateStruct;
+        screen_coord->x = coordinate.x * SCALE + MOVE_IN_X;
+        screen_coord->y = coordinate.y * SCALE + MOVE_IN_Y;
+
+        shape.setPosition(coordinate.x, coordinate.y);
+        orbit_tail->push_front(screen_coord);
+        delete screen_coord;
+
 //        window->draw(shape);
         for (int i = 0; i < orbit_tail->size(); i++) {
             draw(window, orbit_tail->get_item(i), ((float)ORBIT_VISIBLE - (float)i) / (float)ORBIT_VISIBLE * 255);
@@ -226,6 +243,7 @@ public:
 
         acceleration.x += G_CONSTANT * sun->mass / pow(r,3) * (sun->coordinate.x - coordinate.x);
         acceleration.y -= G_CONSTANT * sun->mass / pow(r,3) * (coordinate.y - sun->coordinate.y);
+
     }
 
     void reset_acceleration() {
@@ -251,33 +269,57 @@ public:
 
 };
 
+using namespace std;
 
 int main()
 {
     auto window = sf::RenderWindow({1920, 1080}, "CMake SFML Project");
     window.setFramerateLimit(FPS);
 
-    Planet sun((window.getSize().x / 2.f), (window.getSize().y / 2.f), 1.98e+30,
+    // setting coordinates to the center of a screen
+    MOVE_IN_X = window.getSize().x / 2.f;
+    MOVE_IN_Y = window.getSize().y / 2.f;
+
+
+    // initializing planets
+    Planet sun(0, 0, 1.98e+30,
                sf::Color(255,255,0, 255),4.f, false);
 
-    Planet earth((window.getSize().x / 4.f), (window.getSize().y / 2.f), 0, 8e+13,  5.976e+24,
-                 sf::Color(0, 0, 255), 2.f, true);
+//    Planet earth(1.49e+11, 0, 0, 0,  5.976e+24,
+//                 sf::Color(0, 0, 255), 2.f, true);
+
+    Planet satellite(7.78e+11 + 6e+7, 0, 0, sf::Color(0,250,0), 1.f, true);
+
+    SCALE = MOVE_IN_Y / 7.78e+11;
+
+    Planet jupiter(7.78e+11, 0, 0, 0,  5.976e+24,
+                 sf::Color(0, 0, 255), 1.f, true);
+    int num_of_bodies = 3;
+    Planet* calcs[3] = {&sun,&satellite, &jupiter, };
+
 
     std::cout<<(window.getSize().x / 4.f)<<std::endl;
-    earth.round_orbit(&sun);
-    earth.velocity.y/=3;
+    jupiter.round_orbit(&sun);
+    satellite.round_orbit(&jupiter);
 
-    std::cout << earth.velocity.x<< ' '<<earth.velocity.y << std::endl;
+    satellite.velocity.x += jupiter.velocity.x;
+    satellite.velocity.y += jupiter.velocity.y;
 
-    double t = 1e-14;
+
+
+    // time passed in one frame
+    double t = 50000;
+
+    std::cout << std::endl << "Following parameters of the simulation are set up by default:" << std::endl;
+    std::cout << "Scale (meters per pixel): " << 1/SCALE << '\t' << "Time acceleration: " << t * FPS << std::endl;
+
+//    cout<< earth.velocity.x  << ' ' << earth.velocity.y << endl;
+    cout << satellite.velocity.x << ' ' << satellite.velocity.y << endl;
 //    sun.move(&window, 0,0);
     while (window.isOpen())
     {
-        for (auto event = sf::Event(); window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-
+        for (auto event = sf::Event(); window.pollEvent(event);) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
             }
 
@@ -285,23 +327,14 @@ int main()
 
         window.clear(sf::Color::Black);
 
-
-
-        earth.calculate_acceleration(&sun);
-        earth.calculate_velocity(&window, t);
-        earth.reset_acceleration();
-        sun.calculate_velocity(&window, t);
-
-//        std::cout << earth.coordinate.x << ' ' << earth.coordinate.y << ' ' << ' ' <<
-//        earth.velocity.x << ' ' << earth.velocity.y << std::endl;
-
-//        auto shape1 = new sf::CircleShape;
-//        *shape1 = shape;
-//        shape1->move(0.1*t,0.1);
-//        window.draw(*shape1);
-//
-//        delete shape1;
-//        t++;
+        for (int i = 0; i < num_of_bodies; i++) {
+            for (int j = 0; j < num_of_bodies; j++)
+                if (i != j) {
+                    calcs[i]->calculate_acceleration(calcs[j]);
+                }
+            calcs[i]->calculate_velocity(&window, t);
+            calcs[i]->reset_acceleration();
+        }
 
         window.display();
     }
